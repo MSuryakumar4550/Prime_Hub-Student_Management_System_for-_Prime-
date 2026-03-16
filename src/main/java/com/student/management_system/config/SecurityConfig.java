567cdf20f953f726ae9a59e67b7a2f82f4f2f938
MSuryakumar4550 <muvsuryakumar@gmail.com>
2026-03-16 19:01:34 +0530

    Update SecurityConfig.java

--- src/main/java/com/student/management_system/config/SecurityConfig.java
+++ src/main/java/com/student/management_system/config/SecurityConfig.java
@@ -108,7 +108,7 @@ public class SecurityConfig {
         CorsConfiguration config = new CorsConfiguration();
         config.setAllowedOriginPatterns(List.of(
                 "https://prime-hub-frontend-official.vercel.app",
-                "http://localhost:5173",
+                "http://localhost:5173"
         ));
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH"));
         config.setAllowedHeaders(List.of("Authorization", "Content-Type", "Accept"));
